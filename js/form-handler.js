91b23d714487b26fef773236ee76dd15cd30b76b
Whitney <whitneywishart@gmail.com>
2017-05-06 15:38:50 -0700

    clean up js

--- js/form-handler.js
+++ js/form-handler.js
@@ -1,9 +1,8 @@
-console.log("I am the handler of forms");
+// console.log("I am the handler of forms");
 
 function capture() {
   event.preventDefault();
-  console.log(document.customerInfo.email.value);
-  var email = document .customerInfo.email.value;
+  var email = document.customerInfo.email.value;
 
-  console.log("Thanks for submitting your email, " + email + "!");
+  console.log("Thanks for signing up for our mailing list, " + email + "!");
 }
