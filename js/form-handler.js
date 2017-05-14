f191bf7ea6eb7876b33727479748c79961419fba
Whitney <whitneywishart@gmail.com>
2017-05-14 11:14:42 -0700

    add add remove from cart images

--- js/form-handler.js
+++ js/form-handler.js
@@ -6,3 +6,14 @@ function capture() {
 
   console.log("Thanks for signing up for our mailing list, " + email + "!");
 }
+
+var cart = [
+  { name: "Reversible Plaid", price: 26.99, id: "item01"}
+  { name: "Fringed Plaid", price: 18.99, id: "item02"}
+  { name: "Multi Color", price: 22.99, id: "item03"}
+  { name: "Northern Lights", price: 29.99, id: "item04"}
+  { name: "Ombre Infinity", price: 11.99, id: "item05"}
+  { name: "Ashby Twill", price: 70.99, id: "item06"}
+  { name: "Wool Cable Knit", price: 49.99, id: "item07"}
+  { name: "Etro Paisley-Print Silk", price: 26.99, id: "item08"}
+]
